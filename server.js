ca660f91f10bb9f39037d5b9adfb43196f40f0eb
katerz avocado <ktrinaapodaca@yahoo.com>
2021-09-08 14:29:30 -0600

    some changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
-const sequelize = require("./config/connection");
+// const sequelize = require("./config/connection");
 const routes = require("./controllers");
 // const helpers = require("./utils/helpers");
 
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3001;
 
 // const hbs = exphbs.create({ helpers });
 
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening on port 3001"));
-});
+// sequelize.sync({ force: false })
+// .then(() => {
+app.listen(PORT, () => console.log("Now listening on port 3001"));
+// });
