bd63f7d464b470edf7ec88aa55f379e8b56a43a3
Korbin Sargent <korbinksargent@gmail.com>
2021-09-08 13:33:29 -0600

    updated route / review

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
 const sequelize = require("./config/connection");
-// const routes = require("./controllers");
+const routes = require("./controllers");
 // const helpers = require("./utils/helpers");
 
 const app = express();
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening on port 3001"));
