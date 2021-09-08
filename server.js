6526374efd124031e98c7928a243a7db2b2144f9
James Broski <james.broski.5@gmail.com>
2021-09-08 10:16:13 -0600

    models and config

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 //Testing first push - Hayden
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+const sequelize = require("./config/connection");
+const routes = require("./controllers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
