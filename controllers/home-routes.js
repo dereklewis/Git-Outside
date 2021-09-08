3d235a7820b2224a2344ef60ffe38b5e65bc7413
James Broski <james.broski.5@gmail.com>
2021-09-08 10:48:33 -0600

    server running

---
+++ controllers/home-routes.js
@@ -0,0 +1,19 @@
+const router = require("./api/review-routes");
+
+// router.get("/", withAuth, async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       attributes: { exclude: ["password"] },
+//       order: [["name", "ASC"]],
+//     });
+
+//     const users = userData.map((project) => project.get({ plain: true }));
+
+//     res.render("homepage", {
+//       users,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
