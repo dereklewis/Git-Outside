cfba7821779d22d28bf99b2a6cd549f0fbaba8b9
Korbin Sargent <korbinksargent@gmail.com>
2021-09-09 11:21:01 -0600

    Updated ser,js with auth and sess

--- db/schema.sql
+++ db/schema.sql
@@ -4,15 +4,15 @@ CREATE DATABASE campground_db;
 USE campground_db;
 
 
-CREATE TABLE campground (
-    id INT NOT NULL AUTO_INCREMENT,
-    campground_name VARCHAR(30) NOT NULL,
-    state VARCHAR(2) NOT NULL,
-    city VARCHAR(30) NOT NULL,
-    amenities BOOLEAN NOT NULL,
-    accessibility BOOLEAN NOT NULL,
-    price VARCHAR(12) NOT NULL,
-    review_text VARCHAR(500)
-    rating INT NOT NULL, 
-    PRIMARY KEY(state)
-);
+-- CREATE TABLE campground (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     campground_name VARCHAR(30) NOT NULL,
+--     state VARCHAR(2) NOT NULL,
+--     city VARCHAR(30) NOT NULL,
+--     amenities BOOLEAN NOT NULL,
+--     accessibility BOOLEAN NOT NULL,
+--     price VARCHAR(12) NOT NULL,
+--     review_text VARCHAR(500)
+--     rating INT NOT NULL, 
+--     PRIMARY KEY(id)
+-- );
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
-// const sequelize = require("./config/connection");
+const sequelize = require("./config/connection");
 const routes = require("./controllers");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 // const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
@@ -19,7 +33,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-// sequelize.sync({ force: false })
-// .then(() => {
-app.listen(PORT, () => console.log("Now listening on port 3001"));
-// });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening on port 3001"));
+});
